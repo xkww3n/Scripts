fab858da9763c4728d01586c57008646783cd264
xkww3n <xkww3n@outlook.com>
2023-08-16 13:01:50 +0800

    Improve notify

--- google-api-availability/google-api-availability.js
+++ google-api-availability/google-api-availability.js
@@ -3,13 +3,13 @@ $httpClient.get('https://timeline.google.com/', (error,response) => {
         console.log(error)
     }
     if(response.statusCode === 400) {
-        var result = "不可用"
-        var color = "#db4437"
+        var result = "受限\nYoutube Premium、时间轴等服务将不可用"
+        var color = "#DB4437"
         var logo = "location.slash"
     }
     else {
-        var result = "正常"
-        var color = "#4285f4"
+        var result = "正常\n可以享受全部 Google 服务"
+        var color = "#4285F4"
         var logo = "location.fill.viewfinder"
     }
     $done({
