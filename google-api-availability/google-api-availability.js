c1fc4bf9918fd592a03079b89a06c53db96ca47a
xkww3n <xkww3n@outlook.com>
2023-08-16 15:24:59 +0800

    Add error handling, adjust icon and color.

--- google-api-availability/google-api-availability.js
+++ google-api-availability/google-api-availability.js
@@ -2,15 +2,20 @@ $httpClient.get('https://timeline.google.com/', (error,response) => {
     if(error) {
         console.log(error)
     }
-    if(response.statusCode === 400) {
+    if(response.statusCode === 200) {
+        var result = "正常\n可以享受全部 Google 服务"
+        var color = "#0F9D58"
+        var logo = "location"
+    }
+    else if(response.statusCode === 400) {
         var result = "受限\nYoutube Premium、时间轴等服务将不可用"
-        var color = "#DB4437"
-        var logo = "location.slash"
+        var color = "#F4B400"
+        var logo = "location.viewfinder"
     }
     else {
-        var result = "正常\n可以享受全部 Google 服务"
-        var color = "#4285F4"
-        var logo = "location.fill.viewfinder"
+        var result = "错误\n到 Google 服务器的连接出现问题"
+        var color = "#DB4437"
+        var logo = "location.slash"
     }
     $done({
         content: result,
